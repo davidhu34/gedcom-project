b11ed56f60e06a8b625352866efd7efd0b5c8f45
David Hu <b01901133@gmail.com>
2021-03-02 11:23:15 -0500

    change project 3 output

--- features/project3.py
+++ features/project3.py
@@ -1,30 +1,80 @@
 
-from typing import List
+from typing import Any, List
 from datetime import date as Date
 from prettytable import PrettyTable
 from gedcom import GedcomRepository, prompt_repository_file
 
+NA: str = 'NA'
+
+
+def optional_string_display(data: Any) -> str:
+    ''' return string display of data '''
+    if not data:
+        # return NA if data is invalid
+        return NA
+
+    return f'{data}'
+
 
 def id_list_display(id_list: List[str]) -> str:
+    ''' get ID list display '''
+    if not id_list:
+        # return NA if list is empty or invalid
+        return NA
+
+    # ID's are in quotes
+    # join by comma
     content: str = ", ".join([f"'{id}'" for id in id_list])
+    # wrapped by brackets
     return f'{{{content}}}'
 
 
-def print_gedcom_info() -> None:
-    ''' project 3 modifications from project 2 '''
-    repo: GedcomRepository = prompt_repository_file()
-
+def print_gedcom_info(repository: GedcomRepository) -> None:
+    ''' project 3 pretty tables '''
     individual_fields: List[str] = [
         'ID',
         'Name',
+        'Gender',
+        'Birthday',
+        'Age',
+        'Alive',
+        'Death',
+        'Child',
+        'Spouse'
     ]
     individual_table: PrettyTable = PrettyTable(field_names=individual_fields)
 
-    # individuals sorted by ID are available in GedcomRepository property
-    for individual in repo.individuals:
+    # add rows from university students
+    for individual in repository.individuals:
+
+        birth_date: Date = individual.birth
+        today: Date = Date.today()
+        this_birth_date: Date = Date(
+            today.year, birth_date.month, birth_date.day)
+        age: int = today.year - birth_date.year - \
+            (1 if today < this_birth_date else 0)
+
+        death_date: Date = individual.death
+        alive: bool = not death_date
+
+        is_male: bool = individual.sex == 'Y'
+        spouse_of_families: List[str] = [repository.family[family_id]
+                                         for family_id in individual.spouse_of_list]
+        spouses: List[str] = [
+            family.wife if is_male else family.husband for family in spouse_of_families]
+        children: List[str] = [
+            child for family in spouse_of_families for child in family.children]
+
         individual_table.add_row([
             individual.id,
             individual.name,
+            individual.sex,
+            f'{birth_date}',
+            age,
+            alive,
+            optional_string_display(death_date),
+            id_list_display(children),
+            id_list_display(spouses),
         ])
 
     print('Individuals')
@@ -32,25 +82,34 @@ def print_gedcom_info() -> None:
 
     family_fields: List[str] = [
         'ID',
+        'Married',
+        'Divorced',
         'Husband ID',
         'Husband Name',
         'Wife ID',
         'Wife Name',
+        'Children',
     ]
     family_table: PrettyTable = PrettyTable(field_names=family_fields)
 
-    # families sorted by ID are available in GedcomRepository property
-    for family in repo.families:
+    for family in repository.families:
 
-        husband_name: str = repo.individual[family.husband].name
-        wife_name: str = repo.individual[family.wife].name
+        husband_id: str = family.husband
+        husband_name: str = repository.individual[husband_id].name
+        wife_id: str = family.wife
+        wife_name: str = repository.individual[wife_id].name
+
+        children
 
         family_table.add_row([
             family.id,
-            family.husband,
-            repo.individual[family.husband].name,
-            family.wife,
-            repo.individual[family.wife].name,
+            f'{family.marriage}',
+            optional_string_display(family.divorce),
+            husband_id,
+            husband_name,
+            wife_id,
+            wife_name,
+            id_list_display(family.children),
         ])
 
     print('Families')
--- index.py
+++ index.py
@@ -1,7 +1,9 @@
 
 # from features.project2 import validate_gedcom_file
 from features.project3 import print_gedcom_info
+from gedcom import GedcomRepository, prompt_repository_file
 
 if __name__ == "__main__":
-    # validate_gedcom_file()
-    print_gedcom_info()
+    repo: GedcomRepository = prompt_repository_file()
+
+    print_gedcom_info(repo)
