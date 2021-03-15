955a64e47ec2640531eefa9e4d6d8109c95a4f99
David Hu <b01901133@gmail.com>
2021-03-15 15:05:27 -0400

    Revert "unique name validations"
    This reverts commit f882104bfbed3420bf27e961c1a1e6a94575a579.

--- index.py
+++ index.py
@@ -2,12 +2,11 @@ from gedcom import GedcomRepository, prompt_repository_file
 from features.project3 import print_gedcom_info
 from features.family_role_validation import correct_gender_roles, unique_family_spouses
 
-from adsfl;asdkf
+
 if __name__ == "__main__":
     repo: GedcomRepository = prompt_repository_file()
 
     repo \
         .validate(correct_gender_roles) \
         .validate(unique_family_spouses) \
-        .validate(asd;flkasd) \
         .showcase(print_gedcom_info)
