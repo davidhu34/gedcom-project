7ff55f1dd97a758813f54c7f512c66813f406990
Jack Bittker <32401263+JackBittker@users.noreply.github.com>
2021-03-17 14:19:53 -0400

    Added Marriage/Death Test files

---
+++ features/birth_before_test.py
@@ -0,0 +1,23 @@
+from gedcom.testing import GedcomTestCase
+from features.birth_before import birth_before_death,birth_before_marriage
+
+
+class UniquenamefirstnamesandbirthdatevalidationsTest(GedcomTestCase):
+
+    def test_birth_before_marriage(self) -> None:
+        """ test birth before marriage """
+
+        self.assert_file_validation_fails(
+            'incorrect_birth_before_marriage', birth_before_marriage)
+
+        self.assert_file_validation_passes(
+            'correct_birth_before_marriage', birth_before_marriage)
+
+    def test_birth_before_death(self) -> None:
+        """ test birth before death """
+
+        self.assert_file_validation_fails(
+            'incorrect_birth_before_death', birth_before_death)
+
+        self.assert_file_validation_passes(
+            'correct_birth_before_death', birth_before_death)
