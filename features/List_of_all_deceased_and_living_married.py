a43dd2f9601850afae955b8f4881fc3cf02dfdba
Barakap <33730053+Barakap@users.noreply.github.com>
2021-04-05 15:08:02 -0400

    updated US 29 and 30

--- features/List_of_all_deceased_and_living_married.py
+++ features/List_of_all_deceased_and_living_married.py
@@ -4,39 +4,29 @@ from gedcom import GedcomRepository
 from gedcom.pretty_table import pretty_print_individuals
 
 
-def deceased_individual_list (repo):
-    '''US29'''
+def deceased_individual_list(repo):
+    '''US29: List all deceased individuals'''
     deceased_individuals = []
 
     for individual in repo.individuals:
 
-        if individual.death :
+        if individual.death:
             deceased_individuals.append(individual)
-            
-   
-    return 'deceased_individuals' deceased_individuals
+
+    return 'deceased_individuals', deceased_individuals
 
 
-def living_married_list (repo):
-    living_married = []
-    '''US30'''
+def living_married_list(repo):
+    '''US30 : List all living married individuals'''
+    living_married = []  
     for family in repo.families:
 
         if family.marriage and not family.divorce:
 
             if family.husband and not family.husband.death:
-                living_married.append(husband)
+                living_married.append(family.husband)
 
             if family.wife and not family.wife.death:
-                living_married.append(wife)
+                living_married.append(family.wife)
 
     return 'living married', living_married
-
-            
-       
-
-    
-
-
-            
-        
