2e06727222188120f14ee33148ca0744915bc9a8
jacquelineramos8 <jramos8@stevens.edu>
2021-04-12 12:48:24 -0400

    adjust marriage after 14 date comparison

--- features/marriage_after_14_and_male_last_names.py
+++ features/marriage_after_14_and_male_last_names.py
@@ -1,4 +1,5 @@
 from datetime import date as Date
+from prettytable import PrettyTable
 from gedcom import GedcomRepository
 
 def marriage_after_14(repo: GedcomRepository):
@@ -8,24 +9,54 @@ def marriage_after_14(repo: GedcomRepository):
         marriage_date = family.marriage
         husband = family.husband
         wife = family.wife
-        husband_age = marriage_date - husband.birth
-        wife_age = marriage_date - wife.birth
+        husband_14th = husband.birth.replace(year=husband.birth.year+14)
+        wife_14th = wife.birth.replace(year=wife.birth.year+14)
 
         if not marriage_date:
             continue
 
-        # age 14 years is equivalent to 5110 days
-        if husband.birth and (husband_age.days < 5110):
+        if husband.birth and (husband_14th > marriage_date):
             invalid_marr_date_line_no = family.marriage_line_no
             errors.append(
                 f'ERROR US10: Individual({husband.id}) in Family({family.id}) married (at line {invalid_marr_date_line_no}) when under age 14.')
         
-        if wife.birth and (wife_age.days < 5110):
+        if wife.birth and (wife_14th > marriage_date):
             invalid_marr_date_line_no = family.marriage_line_no
             errors.append(
                 f'ERROR US10: Individual({wife.id}) in Family({family.id}) married (at line {invalid_marr_date_line_no}) when under age 14.')
     
     return errors
+
+def male_last_names(repo: GedcomRepository):
+    """ US16 List of all the male last names """
+    male_last_names = []
+    male_ln_dict = {}
+    pt = PrettyTable()
+    pt.field_names = ['ID', 'Last Name']
+    for individual in repo.individuals:
+
+        if individual.sex and individual.sex == 'M':
+            if individual not in male_last_names:
+                male_last_names.append(individual)
+    
+    for item in male_last_names:
+        name_list = item.name.split()
+        last_name = name_list[1]
+        if last_name in male_ln_dict:
+            male_ln_dict[last_name] = male_ln_dict[last_name] + ',' + item.id
+
+        else:
+            male_ln_dict[last_name] = item.id
+
+    for key, value in male_ln_dict.items():
+        pt.add_row([value, key])
+    
+
+    print('Male Last Names', pt)
+
+    return 'Male Last Names', male_last_names
+
+    
 
 
 
--- features/marriage_after_14_and_male_last_names_test.py
+++ features/marriage_after_14_and_male_last_names_test.py
@@ -1,5 +1,5 @@
 from gedcom.testing import GedcomTestCase
-from features.marriage_after_14_and_male_last_names import marriage_after_14
+from features.marriage_after_14_and_male_last_names import marriage_after_14, male_last_names
 
 class MarriageAgeAndMaleLastNamesTest(GedcomTestCase):
 
@@ -13,3 +13,8 @@ class MarriageAgeAndMaleLastNamesTest(GedcomTestCase):
         ])
 
         self.assert_file_validation_passes('correct_marriage_after_14', marriage_after_14)
+    
+    def test_male_last_names(self) -> None:
+        """ test male_last_names """
+
+        self.assert_printer_result('male_last_names', male_last_names, )
