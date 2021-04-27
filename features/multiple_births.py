4e4af0d88c1290608832d2815f243c5f68f47c9a
Jack Bittker <32401263+JackBittker@users.noreply.github.com>
2021-04-27 14:03:04 -0400

    Update US 14/15

--- features/multiple_births.py
+++ features/multiple_births.py
@@ -4,36 +4,30 @@ from gedcom import GedcomRepository
 
 
 def siblings_born_at_same_time(repo: GedcomRepository) -> List[str]:
-  ''' US14: No more than five siblings should be born at the same time '''
-  errors: List[str] = []
-
-  for family in repo.families:
-    
-    children = family.children  # list of indis
-    for child in children:
-        born_at_same_time=1
-        for child2 in children:
-            if child is child2:
-              continue
-            if child.birth ==child2.birth:
-                born_at_same_time+=1
-            if born_at_same_time>5:
+    ''' US14: No more than five siblings should be born at the same time '''
+    errors: List[str] = []
+    for family in repo.families:
+        children=  family.children
+        birth_day_dict= defaultdict(list)
+        for child in children:
+            birth_day_key: str = f'{child.birth}'
+            birth_day_dict[birth_day_key].append(child)
+        for key, same_birth_children in birth_day_dict.items():
+            if len(same_birth_children) > 5:
                 errors.append(
-                f'ERROR US14 at line {child.birth_line_no}: too many siblings born at once in family({family.id})')
-                return
-  return errors
+                f'ERROR US14 at line {child.birth_line_no}: too many siblings born at once({key}) in family({family.id})')
+    return errors
 
 
 def too_many_siblings(repo: GedcomRepository) -> List[str]:
     ''' US15: There should be fewer than 15 siblings in a family '''
     errors: List[str] = []
- 
+
     for family in repo.families:
-        child_count =0
+        child_count = 0
         children = family.children  # list of indis
-        for child in children:
-            child_count+=1
-        if child_count>14:
-            errors.append(f'ERROR US14 at line {child.birth_line_no}: too many siblings in family: ({family.id})')
+        if len(children) > 14:
+            errors.append(
+                f'ERROR US15 at line {family.line_no}: too many siblings in family: ({family.id})')
 
     return errors
